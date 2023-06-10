847f196a3cdee2dc535d451defa54b6e29611c91
Phong <131786469+Phong3011@users.noreply.github.com>
2023-06-10 17:29:08 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+//CRUD
+//Read
+
+const b11Ele = document.getElementById("b11");
+// const btnEles = document.getElementsByClassName("btn") //trả ra 1 array
+// const bodyEles = document.getElementsByTagName("body"); //trả ra array
+
+// const queryDivBtns = document.querySelectorAll(".calculate--body> > .calculate-- number > .btn-number:nth-child(3)")
+// const queryOneEle = document.querySelector(".header_b11");
+
+//update
+//InnerHTML
+// b11Ele.innerHTML = "<span>abc</span>"
+// b11Ele.innerText = "asdsad";
+
+//create
+//cach 1
+// const newParagraph = document.createElement("p")
+// newParagraph.innerText="abc"
+// newParagraph.style.fontsize ="24px"
+// console.log(newParagraph)
+
+
+// const newChildSpan = document.createElement("span")
+// newChildSpan.id="sss"
+// newChildSpan.style.fontsize = "16px"
+// newChildSpan.style.color ="white";
+
+// b11Ele.appendChild(newParagraph);
+// b11Ele.appendChild(newChildSpan)
+
+//cach2
+// b11Ele.innerText +=`
+
+// `
+
+//delete
+// b11Ele.remove()
+
+//event
+// const btnNumbers = document.querySelectorAll(".btn.btn-number");
+
+// function logText(){
+//     console.log("abc");
+// }
+// for(let i = 0 ; i <btnNumbers.length ; i++);{
+//     let innerText = btnNumbers[i].innerText;
+//     btnNumbers[i].addEventListener("click", logText);
+// }
